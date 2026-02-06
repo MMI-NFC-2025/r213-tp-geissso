405ca00ed81e67b830ad1145b1f78d7c0700ce5d
Geiss Solène <solene.geiss@edu.univ-fcomte.fr>
2026-02-06 10:46:04 +0100

    js + pocketbase

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,20 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  image: {
+   remotePatterns: [
+     {
+       protocol: 'http',
+       hostname: '127.0.0.1',
+       port: '8090', 
+     },
+   ],
+ },
   vite: {
     plugins: [tailwindcss()]
   },
 
   adapter: vercel()
+
 });
